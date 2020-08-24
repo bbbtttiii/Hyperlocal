f057c6df582f782248f96dd40d111daa0413efed
bbbtttiii <ben.iburg@gmail.com>
2020-08-24 09:28:26 -0500

    create migrations

--- db/migrate/20200824134912_create_reports.rb
+++ db/migrate/20200824134912_create_reports.rb
@@ -1,6 +1,17 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
+      t.float :temperature
+      t.float :pressure
+      t.float :wind_speed
+      t.string :wind_direction
+      t.float :relative_humidity
+      t.float :precipitaion
+      t.float :cloud_cover
+      t.string :cloud_type
+      t.string :current_conditions
+
+      t.integer :user_id
 
       t.timestamps null: false
     end
