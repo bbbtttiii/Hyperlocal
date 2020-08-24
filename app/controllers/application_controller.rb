55cc9b73a5ac8a20a7ca5a9d54095f1b5783f886
bbbtttiii <ben.iburg@gmail.com>
2020-08-24 09:33:44 -0500

    add users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
     set :views, 'app/views'
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
