6510915621a4c955d36da2266f5bcb5be2f4c0a1
bbbtttiii <ben.iburg@gmail.com>
2020-08-27 15:34:24 -0500

    adds additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ User.create(
     username: "a", password: "1"
 )
 
+User.create(
+    username: "b", password: "2"
+)
+
 Report.create(
     user_id: 1,
     location: "St Paul, MN",
@@ -18,5 +22,22 @@ Report.create(
     cloud_cover: 70,
     cloud_type: "Cumulus",
     precipitation: 0,
+    precip_unit: "in"
+)
+
+Report.create(
+    user_id: 2,
+    location: "Wichita, KS",
+    temperature: 82,
+    temp_unit: "f",
+    current_conditions:"Overcast",
+    pressure: 998, pressure_unit: "mb",
+    wind_speed: 13,
+    wind_direction: "S",
+    wind_unit: "mph",
+    relative_humidity: 75,
+    cloud_cover: 95,
+    cloud_type: "Stratus",
+    precipitation: 0.7,
     precip_unit: "in"
 )
