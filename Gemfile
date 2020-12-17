0e68bccd9f0cfd1c718af6afb6e5fe403c430272
bbbtttiii <ben.iburg@gmail.com>
2020-12-17 12:49:24 -0600

    heroku updates

--- Gemfile
+++ Gemfile
@@ -5,18 +5,21 @@ gem 'activerecord', '~> 4.2', '>= 4.2.6', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-gem 'pg', :group => :production
-gem 'sqlite3', :group => :development
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
-gem 'tux'
 gem 'sinatra-flash'
+gem 'pg', '~> 1.2.3'
 
 group :test do
   gem 'rspec'
   gem 'capybara'
   gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+end
+
+group :development do
+  gem 'sqlite3', '~> 1.3.6' 
+  gem 'tux'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,38 +1,17 @@
+require 'sinatra'
 require 'bundler/setup'
+
 Bundler.require
 
-configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-  require 'bundler/setup'
-  Bundler.require(:default, ENV['SINATRA_ENV'])
-  ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-  )
-end
-
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-  
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
   ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
-  )
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+    )
 end
-
-
-# ENV['SINATRA_ENV'] ||= "development"
-
-# require 'bundler/setup' #finds all gems and adds to load path
-# Bundler.require(:default, ENV['SINATRA_ENV']) #requires gems and deployment envirionment hash (for AR)
-
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "postgresql", #name of db system
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite" #path to db
-# )
-
-# require_all 'app' #loads all files under app folder
